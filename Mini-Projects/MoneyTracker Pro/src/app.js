b1d85cc3c00a73601c8522be1bcd348b2f8a5c96
zaryab02-eng <mohdzaryab99@gmail.com>
2025-09-13 13:50:44 +0530

    working on js

--- Mini-Projects/MoneyTracker Pro/src/app.js
+++ Mini-Projects/MoneyTracker Pro/src/app.js
@@ -2,6 +2,27 @@ const amountInput = document.getElementById("amount");
 const addButton = document.getElementById("addButton");
 const display = document.getElementById("display");
 
+const expenses = [];
+
+amountInput.addEventListener("keydown", (e) => {
+  if (e.key === "-" || e.key === "e") {
+    e.preventDefault();
+    amountInput.placeholder = "Only positive numb allowed";
+    amountInput.classList.add("placeholder:text-red-400", "border-red-400");
+    setTimeout(() => {
+      amountInput.classList.remove(
+        "placeholder:text-red-400",
+        "border-red-400"
+      );
+      amountInput.placeholder = "Please enter an amount";
+    }, 3000);
+  }
+  if (e.key === "Enter") {
+    e.preventDefault();
+    addButton.click();
+  }
+});
+
 addButton.addEventListener("click", () => {
   const inputAmount = amountInput.value.trim();
   if (inputAmount === "") {
@@ -17,12 +38,31 @@ addButton.addEventListener("click", () => {
         "border-red-400",
         "bg-white"
       );
-      amountInput.classList.add("placeholder:text-black", "bg-white");
+      amountInput.classList.add(
+        "placeholder:text-black",
+        "bg-white",
+        "font-serif",
+        "text-[14px]"
+      );
       amountInput.placeholder = "Enter Your Amount";
     }, 2000);
     return;
   }
+  const amount = parseFloat(inputAmount);
+  if (amount <= 0) {
+    amountInput.value = ""; // clear the "0"
+    amountInput.placeholder = "Please enter a valid number";
+    amountInput.classList.add(
+      "placeholder:text-red-400",
+      "border-red-400",
+      "bg-white",
+      "font-serif",
+      "text-[14px]"
+    );
+    return;
+  }
 
+  expenses.push(amount);
   const li = document.createElement("li");
   li.classList.add(
     "flex",
