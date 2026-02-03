4e852a5272194a6cf14068d58008758bbb129895
zaryab02-eng <mohdzaryab99@gmail.com>
2026-02-03 22:33:44 +0530

    fixing

--- projects/Ai Escape/vite.config.js
+++ projects/Ai Escape/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   plugins: [
     react(),
     VitePWA({
